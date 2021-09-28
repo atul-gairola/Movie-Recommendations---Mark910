7b10cb0e020399e2574472de186f1ea765f10fa9
atul-gairola <atulgairola08@gmail.com>
2021-09-28 18:17:11 +0530

    feat: add data

---
+++ src/data.js
@@ -0,0 +1,98 @@
+export default data = {
+  action: [
+    {
+      name: "The Dark Knight",
+      description:
+        "When the menace known as the Joker wreaks havoc and chaos on the people of Gotham, Batman must accept one of the greatest psychological and physical tests of his ability to fight injustice.",
+      rating: 9,
+    },
+    {
+      name: "The Lord of the rings: The return of the king",
+      description:
+        "Gandalf and Aragorn lead the World of Men against Sauron's army to draw his gaze from Frodo and Sam as they approach Mount Doom with the One Ring.",
+      rating: 8.9,
+    },
+    {
+      name: "Inception",
+      description:
+        "A thief who steals corporate secrets through the use of dream-sharing technology is given the inverse task of planting an idea into the mind of a C.E.O.",
+      rating: 8.8,
+    },
+    {
+      name: "The Matrix",
+      description:
+        "When a beautiful stranger leads computer hacker Neo to a forbidding underworld, he discovers the shocking truth--the life he knows is the elaborate deception of an evil cyber-intelligence.",
+      rating: 8.7,
+    },
+    {
+      name: "Gladiator",
+      description:
+        "A former Roman General sets out to exact vengeance against the corrupt emperor who murdered his family and sent him into slavery.",
+      rating: 8.5,
+    },
+  ],
+  horror: [
+    {
+      name: "The Shining",
+      description:
+        "A family heads to an isolated hotel for the winter where a sinister presence influences the father into violence, while his psychic son sees horrific forebodings from both past and future.",
+      rating: 8.4,
+    },
+    {
+      name: "Tumbbad",
+      description:
+        "A mythological story about a goddess who created the entire universe. The plot revolves around the consequences when humans build a temple for her first-born.",
+      rating: 8.3,
+    },
+    {
+      name: "The Exorcist",
+      description:
+        "When a 12-year-old girl is possessed by a mysterious entity, her mother seeks the help of two priests to save her.",
+      rating: 8,
+    },
+    {
+      name: "Get out",
+      description:
+        "A young African-American visits his white girlfriend's parents for the weekend, where his simmering uneasiness about their reception of him eventually reaches a boiling point.",
+      rating: 7.7,
+    },
+    {
+      name: "Evil Dead",
+      description:
+        "The lone survivor of an onslaught of flesh-possessing spirits holes up in a cabin with a group of strangers while the demons continue their attack.",
+      rating: 7,
+    },
+  ],
+  adventure: [
+    {
+      name: "Free Guy",
+      description:
+        "A bank teller discovers that he's actually an NPC inside a brutal, open world video game.",
+      rating: 7.4,
+    },
+    {
+      name: "The Suicide Squad",
+      description:
+        "Supervillains Harley Quinn, Bloodsport, Peacemaker and a collection of nutty cons at Belle Reve prison join the super-secret, super-shady Task Force X as they are dropped off at the remote, enemy-infused island of Corto Maltese.",
+      rating: 7.3,
+    },
+    {
+      name: "Spiderman: No way home",
+      description:
+        "For the first time in the cinematic history of Spider-Man, our friendly neighborhood hero is unmasked and no longer able to separate his normal life from the high-stakes of being a Super Hero. When he asks for help from Doctor Strange, the stakes become even more dangerous, forcing him to discover what it truly means to be Spider-Man.",
+      rating: 7,
+    },
+    {
+      name: "Venom",
+      description:
+        "A failed reporter is bonded to an alien entity, one of many symbiotes who have invaded Earth. But the being takes a liking to Earth and decides to protect it.",
+      rating: 6.7,
+    },
+    {
+      name: "Jungle Cruise",
+      description:
+        "Based on Disneyland's theme park ride where a small riverboat takes a group of travelers through a jungle filled with dangerous animals and reptiles but with a supernatural element.",
+      rating: 6.6,
+    },
+  ],
+};
